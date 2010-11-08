af1cd3d26c1cc30c7a5dfa7e68d1cf0985f8f70d
Aleksandr Rybalko <ray@ddteam.net>
2010-11-09 00:18:06 +0200

    basic Makefiles for devices profiles

---
+++ boards/boards.mk
@@ -0,0 +1,13 @@
+TARGET_DEVICE?="NONE"
+
+.if exists(${ZROUTER_ROOT}/boards/${TARGET_VENDOR}/${TARGET_DEVICE}/)
+TARGET_DEVICEDIR= ${ZROUTER_ROOT}/boards/${TARGET_VENDOR}/${TARGET_DEVICE}
+.include "${TARGET_DEVICEDIR}/board.mk"
+.endif
+
+.if !defined(TARGET_DEVICEDIR)
+PAIRS!=ls -d ${ZROUTER_ROOT}/boards/*/* | sed 's/^.*\/boards\///'
+
+.warning "No board configuration for pair TARGET_VENDOR/TARGET_DEVICE `${TARGET_VENDOR}/${TARGET_DEVICE}`"
+.error "Posible pairs ${PAIRS}"
+.endif
