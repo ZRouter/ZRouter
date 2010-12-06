3d75874c7041f37edea0831f516a3454c0341317
ray <ray@localhost>
2010-12-07 00:36:00 +0200

    RUN.sh logger

--- profiles/zhttpd/profile.mk
+++ profiles/zhttpd/profile.mk
@@ -1,5 +1,5 @@
 
 # XXX path must not be hardcoded, or must point to zhttpd in ports tree
-WORLD_SUBDIRS_PORTS+=/home/ray/work/DDTeam.net/ZRouter/zhttpd-port/
+WORLD_SUBDIRS_PORTS+=/home/ray/work/DDTeam.net/ZRouter/ZHttpd/freebsd-port/
 
 
