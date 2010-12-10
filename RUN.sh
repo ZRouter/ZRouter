7c0994792a7e87ca1459922c03d6d99005782a1b
ray <ray@localhost>
2010-12-10 18:33:05 +0200

    ports seems worked, now require to implement per port config

--- RUN.sh
+++ RUN.sh
@@ -7,7 +7,9 @@ FREEBSD_SRC_TREE=/usr/home/ray/work/@HG/http_my_ddteam_net_hg_BASE_/head/
 else
 FREEBSD_SRC_TREE=/usr/1/MIPS_FreeBSD/HEAD/head
 fi
+DATE=`date +%Y-%m-%d_%H:%M`
 
-make TARGET_VENDOR=D-Link TARGET_DEVICE=DIR-320 FREEBSD_SRC_TREE=${FREEBSD_SRC_TREE} TARGET_PROFILES="SMALL_ openvpn zhttpd" 2>&1 | tee $0.log
+
+make TARGET_VENDOR=D-Link TARGET_DEVICE=DIR-320 FREEBSD_SRC_TREE=${FREEBSD_SRC_TREE} TARGET_PROFILES="SMALL_ openvpn zhttpd" 2>&1 | tee $0.${DATE}.log
 #make TARGET_VENDOR=D-Link TARGET_DEVICE=DIR-320 FREEBSD_SRC_TREE=${FREEBSD_SRC_TREE} TARGET_PROFILES="SMALL_ openvpn" port-build
 #make TARGET_VENDOR=D-Link TARGET_DEVICE=DIR-320 FREEBSD_SRC_TREE=${FREEBSD_SRC_TREE} TARGET_PROFILES="SMALL_ openvpn"    port-build-depend-host PORT_BUILD_DEPEND_HOST=/usr/ports/security/openscep 
