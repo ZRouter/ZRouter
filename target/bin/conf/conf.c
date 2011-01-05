0f3482cdf2e2ba9e9bd52663cc29d44b3f24675a
Aleksandr Rybalko <ray@ddteam.net>
2011-01-05 12:25:10 +0200

    correct json tests one fialed/one passed

--- target/bin/conf/conf.c
+++ target/bin/conf/conf.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 #include <json/json.h>
 
@@ -13,6 +19,7 @@ void bp()
 int main(int argc, char ** argv)
 {
 	json_object *obj;
+	struct stat sb;
 
 	MC_SET_DEBUG(1);
 
@@ -35,20 +42,39 @@ int main(int argc, char ** argv)
 
 	if (argc < 2) return 1;
 
-        char *input = (char*)malloc(1024);
+        char *input;
 
-	FILE *f = fopen(argv[1], "r");
-        while (fgets(input, 1024, f) != NULL) {
-                obj = json_tokener_parse(input);
-                if (is_error(obj)) {
-                        printf("error parsing json: %s\n",
-			       json_tokener_errors[-(unsigned long)obj]);
-                } else {
-                        printf("%s\n", json_object_to_json_string(obj));
-                        json_object_put(obj);
-                }
+	if (stat(argv[1], &sb)) {
+		// perror
+		exit(1);
+	}
+	int f = open(argv[1], O_RDONLY);
+	if (f < 0 ) {
+		// perror
+		exit(1);
+	}
+	input = mmap(0, sb.st_size, PROT_READ, 0, f, 0);
+	// Check "input"
+	printf(
+	    "===========================================\n"
+	    " Dump:\n"
+	    "===========================================\n"
+	    "%s\n"
+	    "===========================================\n",
+	    input
+	);
+
+        obj = json_tokener_parse(input);
+        if (is_error(obj)) {
+                printf("error parsing json: %s\n",
+		       json_tokener_errors[-(unsigned long)obj]);
+        } else {
+                printf("%s\n", json_object_to_json_string(obj));
+                json_object_put(obj);
         }
-        fclose(f);
+
+        munmap(input, sb.st_size);
+        close(f);
 
 
 	return 0;
