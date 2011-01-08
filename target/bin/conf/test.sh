96ff559fcf122ace08be1b77c74ac6d60907fc5b
Aleksandr Rybalko <ray@ddteam.net>
2011-01-09 00:54:08 +0200

    conf ++++

--- target/bin/conf/test.sh
+++ target/bin/conf/test.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-TEST='./conf -f hash.json glossary.title glossary.GlossDiv.GlossList.1 glossary.GlossDiv.GlossList.2=yyy'
+TEST='./conf -S -f hash.json glossary.title glossary.GlossDiv.GlossList.1 glossary.GlossDiv.GlossList.2=yyy'
 
 echo ${TEST}
 ${TEST}
 
 echo "Tree overwrite, should ignore while user delete childs"
-TEST='./conf -f hash.json glossary.GlossDiv.GlossList.0=yyy'
+TEST='./conf -S -f hash.json glossary.GlossDiv.GlossList.0=yyy'
 
 echo ${TEST}
 ${TEST}
 
 echo "Tree overwrite, should ignore while user delete childs"
 TEST='./conf -S -f hash.json glossary.GlossDiv.GlossList.1=yy1 glossary.GlossDiv.GlossList.2=yy2 glossary.GlossDiv.GlossList.3=yy3'
+
+echo ${TEST}
+${TEST}
+
+echo "Delete childs"
+TEST='./conf -S -D -f hash.json glossary.GlossDiv.GlossList.1'
 
 echo ${TEST}
 ${TEST}
