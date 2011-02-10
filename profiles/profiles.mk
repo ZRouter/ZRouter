44722b7321402cf56e1a2161fb23cf6d69a4580f
ray <ray@localhost>
2011-02-10 02:03:28 +0200

    makefile tune

--- profiles/profiles.mk
+++ profiles/profiles.mk
@@ -7,6 +7,10 @@
 
 .if exists(${ZROUTER_ROOT}/profiles/${profile}/) && exists(${ZROUTER_ROOT}/profiles/${profile}/profile.mk)
 .include "${ZROUTER_ROOT}/profiles/${profile}/profile.mk"
+
+.if exists(${ZROUTER_ROOT}/profiles/${profile}/files)
+ROOTFS_COPY_DIRS+=${ZROUTER_ROOT}/profiles/${profile}/files
+.endif
 .else
 .error "Profile ${profile} not found"
 .endif
