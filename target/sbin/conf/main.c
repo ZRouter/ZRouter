4aee35b846d2aa11699962e74fcb9ec71656f18b
ray <ray@terran.dlink.ua>
2011-04-12 13:28:07 +0300

    config daemon, xml parsing (dirty)

--- target/sbin/conf/main.c
+++ target/sbin/conf/main.c
@@ -67,73 +67,39 @@ dump(Node *top, char * prepend)
 }
 
 
-#if 0
 Node *
 FillTree()
 {
-	Node *root, *ifs, *ports, *iface, *node, *proto;
-	Attr *pseudo, *cmd, *attr;
+	char *test = 
+	"<root>"
+	    "<test default=\"1\" value=\"0\"><some>data</some></test>"
+	    "<interfaces>"
+		"<wlan0>"
+		    "<DHCP descriptions=\"Get network config from DHCP server\">"
+			"<enable default=\"FALSE\" value=\"TRUE\" type=\"BOOL\" descriptions=\"Enabled status\"></enable>"
+		    "</DHCP>"
 
-#ifdef DEBUG
-	root = newNode("root", 0);
-#else
-	root = newNode("", 0);
-#endif
-	appendNewChild(root, "inttest", 0);
-	ifs = appendNewChildDescr(root, "interfaces", "System interfaces", 0);
-	    createConfValue(ifs, "IPADDR", "ipaddr", "192.168.0.254", "192.168.0.1", "IPv4 Address");
-	ports = appendNewChildDescr(root, "switchports", "Ethernet Switch ports", 0);
-	iface = appendNewChildDescr(ifs, "alc0", "Gigabit Ethernet card", 0);
-	    proto = appendNewChildDescr(iface, "static", "Static configuration", 0);
-		createConfValue(proto, "BOOL", "enable", "No", "No", "Enabled status");
-		createConfValue(proto, "CIDR", "ipaddr", "192.168.0.2/24", "192.168.0.1/24", "IPv4 Address");
-		createConfValue(proto, "IPADDR", "gwaddr", "192.168.0.253", "192.168.0.2", "IPv4 Gateway Address");
-		createConfValue(proto, "IPADDR", "dns1addr", "192.168.100.1", "192.168.0.7", "IPv4 DNS1 Address");
-		createConfValue(proto, "IPADDR", "dns2addr", "192.168.100.2", "192.168.0.8", "IPv4 DNS2 Address");
-	    proto = appendNewChildDescr(iface, "DHCP", "Get network config from DHCP server", 0);
-		createConfValue(proto, "BOOL", "enable", "No", "No", "Enabled status");
-		createConfValue(proto, "CIDR", "ipaddr", "192.168.0.254/23", "0.0.0.0/0", "IPv4 Address");
-		createConfValue(proto, "IPADDR", "gwaddr", "192.168.0.253", "0.0.0.0", "IPv4 Gateway Address");
-		createConfValue(proto, "IPADDR", "dns1addr", "192.168.100.1", "0.0.0.0", "IPv4 DNS1 Address");
-		createConfValue(proto, "IPADDR", "dns2addr", "192.168.100.2", "0.0.0.0", "IPv4 DNS2 Address");
-	appendNewChildDescr(ifs, "run0", "802.11n card", 0);
-	appendNewChildDescr(ifs, "lo0", "Local Loopback device", 0);
-	appendNewChildDescr(ifs, "wlan0", "Wireless LAN interface", 0);
-	appendNewChildDescr(ifs, "lagg0", "Link aggrigation interface LACP", 0);
-	appendNewChild(ports, "port0", 0);
+		"</wlan0>"
+		"<alc0>"
+		    "<DHCP>"
+			"<enable value=\"XXX\" value1=\"TRUE\"></enable>"
+		    "</DHCP>"
 
-	/* Pseudo items */
-	/*
-	 * appendChild(appendNewChild(root,  "get", 0), root->firstChild);
-	 * Looks nice, but STP required :))))
-	 */
-	pseudo = newAttr("type", "pseudo");
-	cmd = newAttr("command", "pseudo");
-	addAttr(appendNewChild(root,  "get", pseudo), cmd);
-	addAttr(appendNewChild(root, "list", pseudo), cmd);
-	addAttr(appendNewChild(root, "show", pseudo), cmd);
-
-	return (root);
-}
-#else
-Node *
-FillTree()
-{
-	char *test = "<test default=\"1\" value=\"0\"><some>data</some></test>";
-	Node *testroot;
-	Node *root;
+		"</alc0>"
+	    "</interfaces>"
+	"</root>";
+	Node *testroot = 0;
+	Node *root = 0;
 
 	load_xml(&root, "test.xml");
 
 
-	if (load_xml_buf(&testroot, test, strlen(test)) == 0)
-		appendChild(root, testroot);
+	if (load_xml_buf(&root, test, strlen(test)) == 0) {}
 
-//	dump(testroot, "   ");
+	XMLdump(root);
 
 	return (root);
 }
-#endif
 
 Node *
 findNode(Node *root, char *path)
@@ -141,7 +107,7 @@ findNode(Node *root, char *path)
 	char * subpath, *name, *p;
 	Node *node, *new;
 
-	if (!root)
+	if (!root || !path)
 		return (0);
 
 	printf("%s: path = %s\n", __func__, path);
@@ -200,27 +166,26 @@ main(int argc, char **argv)
 
 	Node *root = FillTree();
 	NodeListPush(cwd, root);
-//	return (0);
 
-	printf("TEXT_DUMP of root:\n");
-	dump(root, "   ");
-	printf("------------------------------------- TEXT_DUMP\n");
-	printf("TEXT_DUMP of root:\n");
-	dump(root, "-->");
-	printf("------------------------------------- TEXT_DUMP\n");
-	printf("XML_DUMP of root:\n");
-	XMLdump(root);
-	printf("------------------------------------- XML_DUMP\n");
+//	printf("TEXT_DUMP of root:\n");
+//	dump(root, "   ");
+//	printf("------------------------------------- TEXT_DUMP\n");
+//	printf("TEXT_DUMP of root:\n");
+//	dump(root, "-->");
+//	printf("------------------------------------- TEXT_DUMP\n");
+//	printf("XML_DUMP of root:\n");
+//	XMLdump(root);
+//	printf("------------------------------------- XML_DUMP\n");
 
-	Node *ip = findNode(root, "root.interfaces.alc0.static.ipaddr");
+	Node *ip = findNode(root, (argc > 1)?argv[1]:"root.interfaces.alc0.static.ipaddr");
 	if (ip) {
 		printf("node found: name=\"%s\"\n", ip->name);
 		printf("XML_DUMP of root.interfaces.alc0.static.ipaddr:\n");
 		XMLdump(ip);
 		printf("------------------------------------- XML_DUMP\n");
-		printf("TEXT_DUMP of root.interfaces.alc0.static.ipaddr:\n");
-		dump(ip, ">  ");
-		printf("------------------------------------- TEXT_DUMP\n");
+//		printf("TEXT_DUMP of root.interfaces.alc0.static.ipaddr:\n");
+//		dump(ip, ">  ");
+//		printf("------------------------------------- TEXT_DUMP\n");
 	}
 
 	freeNode(root);
--- target/sbin/conf/xml.c
+++ target/sbin/conf/xml.c
@@ -17,34 +17,74 @@ struct xml_state {
 	Node *last;
 };
 
+static void
+applyAttrs(Node *parent, char *name, char *value)
+{
+	Node *node;
+
+	if (!parent) return;
+
+	/* Apply to matched attribute */
+	if (parent->firstAttr)
+		for (node = parent->firstAttr; node; node = node->next) {
+			if (strcmp(node->name, name) == 0) {
+				free(node->value);
+				node->value = value;
+				return;
+			}
+		}
+
+	/* Create if no match */
+	addAttr(parent, newAttr(name, value));
+
+	return;
+}
+
+static Node *
+applyNode(Node *parent, char *name)
+{
+	Node *node;
+
+	if (!parent) return 0;
+
+	/* Return matched node */
+	if (parent->firstChild)
+		for (node = parent->firstChild; node; node = node->next) {
+			if (strcmp(node->name, name) == 0) {
+				return (node);
+			}
+		}
+
+	/* Create if no match */
+	node = newNode(name, 0);
+	appendChild(parent, node);
+	return (node);
+}
+
 static void 
 startElement(void *st, const char *el,const char **attr) 
 {
 	int i;
-	Node *node;
+	Node *node = 0, *n;
 	struct xml_state *xst = st;
 
-	/* Create new node */
-	node = newNode(el, 0);
-
-	if (xst->last) {
-		/* Attach new node to upper */
-		appendChild(xst->last, node);
+	node = applyNode(xst->last, el);
+	if (!node) {
+		printf("ERROR: no target node\n");
+		return;
 	}
 
 	/* Set new node as last */
 	xst->last = node;
 
-	if (!xst->root)
-		xst->root = xst->last;
-
-	print_indent(xst->level);
-	printf("<%s", el);
+//	print_indent(xst->level);
+//	printf("<%s", el);
 	for (i = 0; attr[i] && *attr[i]; i+=2) {
-		printf("%s=\"%s\" ", attr[i], attr[i+1]);
-		addAttr(xst->last, newAttr(attr[i], attr[i+1]));
+//		printf(" %s=\"%s\"", attr[i], attr[i+1]);
+//		addAttr(xst->last, newAttr(attr[i], attr[i+1]));
+		applyAttrs(xst->last, attr[i], attr[i+1]);
 	}
-	printf(">\n");
+//	printf(">\n");
 	xst->level++;
 
 }
@@ -55,8 +95,8 @@ endElement(void *st, const char *el)
 	struct xml_state *xst = st;
 
 	xst->level--;
-	print_indent(xst->level);
-	printf("</%s>\n", el);
+//	print_indent(xst->level);
+//	printf("</%s>\n", el);
 
 	if (xst->last == xst->root) return;
 
@@ -79,13 +119,13 @@ static void characters(void *st, const char *ch, int len)
 
 int load_xml(Node **newroot, char *file) 
 {
-	char Buff[BUFFSIZE];
+	char buf[BUFFSIZE];
 	FILE *fp;
 	struct xml_state xst;
 
 	xst.level = 0;
-	xst.root = 0;
-	xst.last = 0;
+
+	xst.last = xst.root = (*newroot)?*newroot:newNode("root", 0);
 
 	fp = fopen(file,"r");
 
@@ -95,11 +135,10 @@ int load_xml(Node **newroot, char *file)
 	XML_SetUserData(parser, &xst);
 
 	for (;;) {
-    		int len  = fread(Buff, 1, BUFFSIZE, fp);
+    		int len  = fread(buf, 1, BUFFSIZE, fp);
     		int done = feof(fp);
-    		if (! XML_Parse(parser, Buff, len, done))
-
-    		return (-1);
+    		if (! XML_Parse(parser, buf, len, done))
+    			return (-1);
 
 		if (done) break;
 	}
@@ -115,8 +154,7 @@ int load_xml_buf(Node **newroot, char *buf, int len)
 	struct xml_state xst;
 
 	xst.level = 0;
-	xst.root = 0;
-	xst.last = 0;
+	xst.last = xst.root = (*newroot)?*newroot:newNode("root", 0);
 
 	XML_Parser parser = XML_ParserCreate(NULL);
 	XML_SetElementHandler(parser, startElement, endElement);
@@ -172,16 +210,15 @@ XMLdumpL(Node *top, int l)
 
 	printf(">");
 
+	if (top->firstChild)
+		/* Break line only if have childs */
+		printf("\n");
+
 	for (node = top->firstChild; node; node = node->next ) {
 
-		{
-			/* Break line only if have childs */
-			printf("\n");
 			XMLdumpL(node, l+1);
 			/* indent close tag only if have childs */
 			print_indent(l); 
-		}
-
 	}
 	printf("</%s>\n", top->name);
 }
