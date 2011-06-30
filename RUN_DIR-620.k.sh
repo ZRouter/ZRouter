3a724a22fd729a7750edea2e1a73f360dfd78a3b
Aleksandr Rybalko <ray@ddteam.net>
2011-06-30 12:47:22 +0300

    DIR-620 kernel helper scripts

---
+++ RUN_DIR-620.k.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+HOST=`hostname`
+echo "'${HOST}'"
+if [ "x${HOST}" = "xrnote.ddteam.net" ] ; then
+FREEBSD_SRC_TREE=/usr/home/ray/work/@HG/http_my_ddteam_net_hg_BASE_/head/
+else
+FREEBSD_SRC_TREE=/usr/1/MIPS_FreeBSD/HEAD/head
+fi
+DATE=`date +%Y-%m-%d_%H:%M`
+
+
+__MAKE_CONF=/dev/null
+SRCCONF=/dev/null
+
+export __MAKE_CONF
+export SRCCONF
+
+echo "build started, enter \`tail -f $0.${DATE}.log\` to view build details"
+make TARGET_PAIR=D-Link/DIR-620 FREEBSD_SRC_TREE=${FREEBSD_SRC_TREE} kernel.oldlzma.uboot 2>&1 | tee $0.${DATE}.log > /dev/null
+#make TARGET_PAIR=D-Link/DIR-620 FREEBSD_SRC_TREE=${FREEBSD_SRC_TREE} kernel.oldlzma.uboot rootfs.iso.ulzma 2>&1 | tee $0.${DATE}.log
+cp /usr/obj/usr/home/ray/work/DDTeam.net/ZRouter/zrouter/D-Link_DIR-620_kernel.oldlzma.uboot /tftpboot/DIR-620/
+#cp /usr/obj/usr/home/ray/work/DDTeam.net/ZRouter/zrouter/D-Link_DIR-620_rootfs_clean.iso.ulzma /tftpboot/DIR-620/
+
+
+echo ${TARGET_PAIR} firmware build done at `date`
+
