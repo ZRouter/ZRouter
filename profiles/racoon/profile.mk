f978bcef0dddc2a24d34ebd0ff48670a04da8d05
Aleksandr Rybalko <ray@ddteam.net>
2011-06-30 12:07:08 +0300

    Add profile that define build racoon1

--- profiles/racoon/profile.mk
+++ profiles/racoon/profile.mk
@@ -1,5 +1,5 @@
 
-WORLD_SUBDIRS_LIB+= ../secure/lib/libcrypto ../secure/lib/libssl
-#WORLD_SUBDIRS_PORTS+=/usr/ports/security/racoon2
-WORLD_SUBDIRS_ZROUTER+=target/sbin/racoon2
+WORLD_SUBDIRS_LIB+=	../secure/lib/libcrypto ../secure/lib/libssl
+#WORLD_SUBDIRS_SBIN+=	setkey
+WORLD_SUBDIRS_ZROUTER+=	target/sbin/racoon
 
