e17bcc4537147386978514886b2629e6eee2e9cd
ray <ray@terran.dlink.ua>
2011-09-01 12:36:26 +0300

    Add DSA-3110-A1 board

---
+++ boards/D-Link/DSA-3110-A1/board.mk
@@ -0,0 +1,67 @@
+
+###################################################
+#
+# Board used hardware/chip`s
+#
+###################################################
+
+
+SOC_VENDOR=Intel
+SOC_CHIP=ixp435
+# Maybe used for kernel config and maybe multiple e.g. "cfi nand"
+BOARD_FLASH_TYPE=cfi
+# TODO: size suffixes
+BOARD_FLASH_SIZE=4194304
+
+
+
+###################################################
+#
+# Vars for kernel config 
+#
+###################################################
+
+# ident 
+KERNCONF_IDENT=${TARGET_VENDOR}_${TARGET_DEVICE}
+# Include usb and SoC usb controller drivers
+WITH_USB=yes
+WITH_IPSEC=yes
+#WITH_WIRELESS=yes
+# Builded modules
+KERNCONF_MODULES_OVERRIDE+=usb/uplcom usb/u3g usb/umodem usb/umass usb/ucom cam zlib atp8620
+
+# Additional utilities
+WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
+
+KERNCONF_OPTIONS+=	ALT_BREAK_TO_DEBUGGER
+KERNCONF_OPTIONS+=	BREAK_TO_DEBUGGER
+
+###################################################
+#
+#       Limits
+#
+###################################################
+
+
+
+# Image must not be biggest than GEOM_MAP_P2 (upgrade part.)
+FIRMWARE_IMAGE_SIZE_MAX=0x007a0000
+
+
+###################################################
+#
+#       Firmware Image Options
+#
+###################################################
+
+TARGET_PROFILES+=SMALL_ mpd ssh dlink.ua.web dhcp mroute ntpdate dnsmasq racoon openvpn ppp hostap
+# nfs_client
+128M!=sh -c 'echo $$((128 * 1024 * 1024))'
+ROOTFS_WITH_KERNEL=yes
+ROOTFS_MEDIA_SIZE?=${128M}
+PACKING_KERNEL_IMAGE?=kernel
+PACKING_ROOTFS_IMAGE?=rootfs
+
+
+IMAGE_SUFFIX=zimage
+
