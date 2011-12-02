f61803f6d6f7bc43189ab6435e70a6c56454eaf5
Aleksandr Rybalko <ray@ddteam.net>
2011-12-02 20:38:55 +0200

    define D-Link DSR-xxx packing tool

--- share/mk/zrouter.local.tools.mk
+++ share/mk/zrouter.local.tools.mk
@@ -24,4 +24,8 @@ ${ZTOOLS_PATH}/asustrx:	${ZTOOLS_PATH}_dir
 	cd ${ZROUTER_ROOT}/tools/asustrx; \
 	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
+${ZTOOLS_PATH}/make_dlink_dsr_image:	${ZTOOLS_PATH}_dir
+	cd ${ZROUTER_ROOT}/tools/make_dlink_dsr_image; \
+	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+
 
