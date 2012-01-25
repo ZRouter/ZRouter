04dd2ab983cd702b5bea947021fba1b4890fd931
ray <ray@terran.dlink.ua>
2012-01-25 11:51:42 +0200

    Fix RS boot

--- boards/Ubiquiti/RS/board.mk
+++ boards/Ubiquiti/RS/board.mk
@@ -29,11 +29,13 @@ WITH_USB=yes
 WITH_IPSEC=yes
 # Builded modules
 # device wlan in kernel alredy enable this modules
-#KERNCONF_MODULES_OVERRIDE+=wlan_xauth wlan_wep wlan_tkip wlan_acl wlan_amrr wlan_ccmp wlan_rssadapt
-#KERNCONF_MODULES_OVERRIDE+=usb/uplcom usb/u3g usb/umodem usb/umass usb/ucom cam zlib
+KERNCONF_MODULES_OVERRIDE+=wlan_xauth wlan_wep wlan_tkip wlan_acl wlan_amrr wlan_ccmp wlan_rssadapt
+KERNCONF_MODULES_OVERRIDE+=usb/uplcom usb/u3g usb/umodem usb/umass usb/ucom cam zlib
 KERNCONF_MODULES_OVERRIDE+=pf pflog
 # Additional utilities
 WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
+
+KERNCONF_DEVICES+=	switch
 
 .if !defined(WITHOUT_WIRELESS)
 KERNCONF_OPTIONS+=	IEEE80211_DEBUG
@@ -66,8 +68,6 @@ KERNCONF_OPTIONS+=	FFS
 #
 ###################################################
 
-
-
 # Image must not be biggest than GEOM_MAP_P2 (upgrade part.)
 FIRMWARE_IMAGE_SIZE_MAX=0x007a0000
 
@@ -79,15 +79,14 @@ FIRMWARE_IMAGE_SIZE_MAX=0x007a0000
 
 TARGET_PROFILES+=SMALL_ ath dnsmasq mpd ssh shttpd dlink.ua.web hostap dhcp \
   racoon ntpdate
-# pf
 
 KERNEL_COMPRESSION=oldlzma
 KERNEL_COMPRESSION_TYPE=oldlzma
 
 MKULZMA_BLOCKSIZE=65536
 
-# XXX: Here is must be Redboot packer, but not defined yet
-PACKING_KERNEL_IMAGE?=kernel.kbin.gz.sync
+UBNT_KERNEL_LOAD_ADDRESS?=0x80050000
+PACKING_KERNEL_IMAGE?=kernel.gz.sync
 PACKING_ROOTFS_IMAGE?=rootfs_clean.iso.ulzma
 
 IMAGE_SUFFIX=zimage
@@ -95,3 +94,4 @@ IMAGE_SUFFIX=zimage
 UBNT_VERSION?=RS.ar7100.Zrouter.V1
 
 NEW_IMAGE_TYPE=ubntimage
+
