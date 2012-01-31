1e3c32ec6c421e0e4142c21eec79412d79f1b573
ray <ray@terran.dlink.ua>
2012-01-31 19:52:02 +0200

    Fix nfs_client profile

--- profiles/nfs_client/profile.mk
+++ profiles/nfs_client/profile.mk
@@ -3,5 +3,6 @@ WORLD_SUBDIRS_SBIN+= mount_nfs
 
 #KERNCONF_OPTIONS+=NFSCLIENT NFS_ROOT
 
-KERNCONF_MODULES_OVERRIDE+=nfsclient nfslock nfs_common krpc
+KERNCONF_MODULES_OVERRIDE+=nfsclient nfslock nfs_common krpc nfscl nfssvc \
+    nfscommon
 
