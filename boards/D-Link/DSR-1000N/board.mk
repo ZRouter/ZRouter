4e59079276200acc53a9620b2b1c78132b1c12bf
Aleksandr Rybalko <ray@ddteam.net>
2012-02-03 17:35:07 +0200

    Update D-Link DSR-1000N setup.

--- boards/D-Link/DSR-1000N/board.mk
+++ boards/D-Link/DSR-1000N/board.mk
@@ -10,8 +10,6 @@ SOC_CHIP=CN5010
 # TODO: size suffixes
 BOARD_FLASH_SIZE=33554432
 
-#?????KERNEL_COMPRESSION=lzma.4.17
-
 ###################################################
 #
 # Vars for kernel config 
@@ -20,16 +18,27 @@ BOARD_FLASH_SIZE=33554432
 KERNCONF_OPTIONS+=	OCTEON_VENDOR_D_LINK
 KERNCONF_OPTIONS+=	OCTEON_BOARD_DSR_1000N
 
+KERNCONF_DEVICES+=	brgphy
 KERNCONF_DEVICES+=	switch
 KERNCONF_DEVICES+=	switch_bcm5325
 
 # ident 
 KERNCONF_IDENT=${TARGET_VENDOR}_${TARGET_DEVICE}
+
 # Include usb and SoC usb controller drivers
 WITH_USB=yes
 WITH_IPSEC=yes
 # Builded modules
-KERNCONF_MODULES_OVERRIDE+=ipfw usb/umass usb/uplcom usb/u3g dummynet
+KERNCONF_MODULES_OVERRIDE+=ipfw dummynet zlib
+KERNCONF_MODULES_OVERRIDE+=usb/uplcom usb/u3g usb/umodem usb/ucom
+KERNCONF_MODULES_OVERRIDE+=usb/umass cam
+
+KERNCONF_MODULES_OVERRIDE+=usb/run usb/rum firmware
+KERNCONF_MODULES_OVERRIDE+=wlan wlan_xauth wlan_wep wlan_tkip wlan_acl \
+    wlan_amrr wlan_ccmp wlan_rssadapt
+
+# Additional utilities ????
+WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
 
 .if !defined(WITHOUT_WIRELESS)
 KERNCONF_MODULES_OVERRIDE+=wlan_xauth wlan_wep wlan_tkip wlan_acl wlan_amrr wlan_ccmp wlan_rssadapt
@@ -40,7 +49,7 @@ KERNCONF_DEVICES+=	wlan
 KERNCONF_DEVICES+=	wlan_amrr
 KERNCONF_DEVICES+=	wlan_wep
 KERNCONF_DEVICES+=	wlan_ccmp
-KERNCONF_DEVICES+=	wlan_tkip
+#KERNCONF_DEVICES+=	wlan_tkip
 
 KERNCONF_OPTIONS+=	AH_DEBUG
 KERNCONF_OPTIONS+=	ATH_DEBUG
@@ -48,7 +57,7 @@ KERNCONF_OPTIONS+=	ATH_DIAGAPI
 KERNCONF_OPTIONS+=	ATH_ENABLE_11N
 KERNCONF_OPTIONS+=	AH_SUPPORT_AR5416
 #KERNCONF_OPTIONS+=	AH_SUPPORT_AR9130
-KERNCONF_OPTIONS+=	AH_RXCFG_SDMAMW_4BYTES
+#KERNCONF_OPTIONS+=	AH_RXCFG_SDMAMW_4BYTES
 # interrupt mitigation not possible on AR9130
 # option		AH_AR5416_INTERRUPT_MITIGATION
 KERNCONF_DEVICES+=	ath
@@ -56,11 +65,6 @@ KERNCONF_DEVICES+=	ath_hal
 KERNCONF_DEVICES+=	ath_pci
 KERNCONF_DEVICES+=	ath_rate_sample
 .endif
-
-
-
-# Additional utilities ????
-WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
 
 ###################################################
 #
@@ -78,7 +82,8 @@ WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
 #
 ###################################################
 
-TARGET_PROFILES+=SMALL_ mpd ssh dlink.ua.web dhcp mroute ntpdate dnsmasq racoon openvpn ppp hostap ath nfs_client
+TARGET_PROFILES+=SMALL_ mpd ssh dlink.ua.web dhcp mroute ntpdate dnsmasq \
+    racoon openvpn ppp hostap ath nfs_client net_help
 
 KERNEL_COMPRESSION=oldlzma
 KERNEL_COMPRESSION_TYPE=oldlzma
@@ -86,7 +91,10 @@ UBOOT_KERNEL_COMPRESSION_TYPE=lzma
 
 MKULZMA_BLOCKSIZE=65536
 
-PACKING_KERNEL_IMAGE?=kernel.kbin.oldlzma.uboot.sync
+# 256K block
+PACKING_KERNEL_ROUND=0x40000
+PACKING_KERNEL_IMAGE?=kernel.strip.gz.sync
 PACKING_ROOTFS_IMAGE?=rootfs_clean.iso.ulzma
 
-NEW_IMAGE_TYPE=split_kernel_rootfs
+NEW_IMAGE_TYPE?=zimage
+
