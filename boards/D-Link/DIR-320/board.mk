9ef38c970674dc465149b1574f0dfea73fc0ce79
ray <ray@terran.dlink.ua>
2012-02-06 14:58:20 +0200

    cleanup. XXX: still unsupported device

--- boards/D-Link/DIR-320/board.mk
+++ boards/D-Link/DIR-320/board.mk
@@ -23,55 +23,10 @@ KERNCONF_IDENT=${TARGET_VENDOR}_${TARGET_DEVICE}
 # Include usb and SoC usb controller drivers
 WITH_USB=yes
 # Builded modules
-KERNCONF_MAKEOPTIONS+=	"MODULES_OVERRIDE=\"ipfw usb/umass usb/uplcom usb/u3g dummynet\""
+KERNCONF_MODULES_OVERRIDE+=	usb/umass usb/uplcom usb/u3g
 
 # Additional utilities
-WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
-
-
-# Flash mapping
-# ifndef GEOM_MAP_P0_PARENT then GEOM_MAP_P0_PARENT=cfid0
-GEOM_MAP_P0_NAME=cfe
-GEOM_MAP_P0_START=0x00000000
-GEOM_MAP_P0_END=0x00030000
-GEOM_MAP_P0_ATTR=RO
-
-GEOM_MAP_P1_NAME=rgdb
-GEOM_MAP_P1_START=0x00030000
-GEOM_MAP_P1_END=0x00040000
-
-GEOM_MAP_P2_NAME=upgrade
-GEOM_MAP_P2_START=0x00040000
-GEOM_MAP_P2_END=0x003e0000
-
-GEOM_MAP_P3_NAME="nvram"
-GEOM_MAP_P3_START=0x003f8000
-GEOM_MAP_P3_END=0x00400000
-
-GEOM_MAP_P4_NAME="bdcfg"
-GEOM_MAP_P4_START=0x003f6000
-GEOM_MAP_P4_END=0x003f8000
-
-GEOM_MAP_P5_NAME="kernel"
-GEOM_MAP_P5_START=0x00040000
-GEOM_MAP_P5_END="search"
-GEOM_MAP_P5.searchkey="--PaCkImGs--"
-GEOM_MAP_P5.searchstart="524384" # 8 64K blocks + AlphaHeaderSize( 96B )
-GEOM_MAP_P5.searchstep="65536"
-
-GEOM_MAP_P6_END=0x003e0000
-GEOM_MAP_P6_NAME="rootfs"
-GEOM_MAP_P6.offset="32"
-GEOM_MAP_P6_START="search"
-GEOM_MAP_P6.searchkey="--PaCkImGs--"
-GEOM_MAP_P6.searchstart="524384" # 8 64K blocks + AlphaHeaderSize( 96B )
-GEOM_MAP_P6.searchstep="65536"
-
-GEOM_MAP_P7_NAME="langpack"
-GEOM_MAP_P7_START=0x003e0000
-GEOM_MAP_P7_END=0x003f0000
-
-
+WORLD_SUBDIRS_ZROUTER+=		target/sbin/upgrade
 
 ###################################################
 #
