f35dcb7507125c5309411dafc094a14b4b411ac9
ray <ray@terran.dlink.ua>
2012-02-06 14:49:26 +0200

    Cleanup + move ipfw from binary set to profiles set.
    Submitted by:   Maciej Milewski

--- boards/Ubiquiti/RS/board.mk
+++ boards/Ubiquiti/RS/board.mk
@@ -31,7 +31,6 @@ WITH_IPSEC=yes
 # device wlan in kernel alredy enable this modules
 KERNCONF_MODULES_OVERRIDE+=wlan_xauth wlan_wep wlan_tkip wlan_acl wlan_amrr wlan_ccmp wlan_rssadapt
 KERNCONF_MODULES_OVERRIDE+=usb/uplcom usb/u3g usb/umodem usb/umass usb/ucom cam zlib
-KERNCONF_MODULES_OVERRIDE+=pf pflog
 # Additional utilities
 WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
 
@@ -78,7 +77,7 @@ FIRMWARE_IMAGE_SIZE_MAX=0x007a0000
 ###################################################
 
 TARGET_PROFILES+=SMALL_ ath dnsmasq mpd ssh shttpd dlink.ua.web hostap dhcp \
-  racoon ntpdate
+  racoon ntpdate pf
 
 KERNEL_COMPRESSION=oldlzma
 KERNEL_COMPRESSION_TYPE=oldlzma
