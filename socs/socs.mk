5aea2a91099d61b415beb3f9af90f93f04bca32b
Aleksandr Rybalko <ray@ddteam.net>
2012-04-17 17:36:55 +0300

    Rename target mipsbe to mips (Upstream changes)

--- socs/socs.mk
+++ socs/socs.mk
@@ -14,7 +14,7 @@ SOC_PAIRS!=ls -d ${ZROUTER_ROOT}/socs/*/* | sed 's/^.*\/socs\///'
 .endif
 
 .if defined(TARGET_ARCH)
-.if ${TARGET_ARCH} == "mipsel" || ${TARGET_ARCH} == "mipseb"
+.if ${TARGET_ARCH} == "mipsel" || ${TARGET_ARCH} == "mips"
 KERNCONF_KERN_LDSCRIPT_NAME?=	ldscript.mips.bin
 .else
 KERNCONF_KERN_LDSCRIPT_NAME?=	ldscript.${TARGET}
