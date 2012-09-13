0dad3c76f004e95c78bd8f2b107d5fc9e56b1070
Aleksandr Rybalko <ray@ddteam.net>
2012-09-14 01:48:26 +0300

    Move local tools objects to ZROUTER_OBJ/... dir.

--- share/mk/zrouter.local.tools.mk
+++ share/mk/zrouter.local.tools.mk
@@ -9,34 +9,34 @@ ${ZTOOLS_PATH}_dir:
 	mkdir -p ${ZTOOLS_PATH}
 
 ${ZTOOLS_PATH}/oldlzma:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/oldlzma; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/oldlzma; mkdir -p ${ZROUTER_OBJ}/`pwd`; \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/packimage:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/packimage ; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/packimage; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/trx:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/trx; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/trx; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/asustrx:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/asustrx; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/asustrx; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/make_dlink_dsr_image:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/make_dlink_dsr_image; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/make_dlink_dsr_image; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/ubnt-mkfwimage:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/ubnt-mkfwimage; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/ubnt-mkfwimage; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/uboot_mkimage:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/uboot_mkimage; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/uboot_mkimage; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
 ${ZTOOLS_PATH}/mktplinkfw:	${ZTOOLS_PATH}_dir
-	cd ${ZROUTER_ROOT}/tools/mktplinkfw; \
-	    MAKEOBJDIRPREFIX=/usr/obj/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
+	cd ${ZROUTER_ROOT}/tools/mktplinkfw; mkdir -p ${ZROUTER_OBJ}/`pwd`;  \
+	    MAKEOBJDIRPREFIX=${ZROUTER_OBJ}/ ${MAKE} ${ZTOOLS_INSTALL_VAR} all install
 
