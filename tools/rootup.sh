f3a222b92702fdfaad24508773a93dec1382ad23
Aleksandr Rybalko <ray@ddteam.net>
2013-08-13 14:11:55 +0300

    Remember about old readlink, which can't do `readlink -f`. Pointed out by:	tinderbox

--- tools/rootup.sh
+++ tools/rootup.sh
@@ -2,10 +2,16 @@
 
 SRCPATH=$1
 
+if [ -x /bin/realpath ] ; then
+	REALPATH="realpath"
+else
+	REALPATH="readlink -f"
+fi
+
 ROOTUP="/.."
 _FOR_ROOTUP=`echo ${SRCPATH} | sed 's/\// /g'`
 for r in ${_FOR_ROOTUP}; do
-	_ROOTUP_TEST=`readlink -f ${SRCPATH}/${ROOTUP}`
+	_ROOTUP_TEST=`${REALPATH} ${SRCPATH}/${ROOTUP}`
 	if [ "x${_ROOTUP_TEST}" != "x/" ] ; then
 		ROOTUP="${ROOTUP}/.."
 	fi
