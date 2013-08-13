bdbf2fb93c0ffb6686ab03eee55de18c040b97c7
Aleksandr Rybalko <ray@ddteam.net>
2013-08-13 12:50:43 +0300

    Replace `realpath` with `readlink -f` so ZRouter.org can build on OpenBSD. Pointed by:	Paul Irofti

--- tools/rootup.sh
+++ tools/rootup.sh
@@ -5,7 +5,7 @@ SRCPATH=$1
 ROOTUP="/.."
 _FOR_ROOTUP=`echo ${SRCPATH} | sed 's/\// /g'`
 for r in ${_FOR_ROOTUP}; do
-	_ROOTUP_TEST=`realpath ${SRCPATH}/${ROOTUP}`
+	_ROOTUP_TEST=`readlink -f ${SRCPATH}/${ROOTUP}`
 	if [ "x${_ROOTUP_TEST}" != "x/" ] ; then
 		ROOTUP="${ROOTUP}/.."
 	fi
