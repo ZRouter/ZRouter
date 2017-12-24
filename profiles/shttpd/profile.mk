964fb7c30bdbee5f9789fb5ad94a64be0caca3a2
Hiroki Mori <yamori813@yahoo.co.jp>
2017-12-24 17:05:16 +0900

    shttpd change to ports from target

--- profiles/shttpd/profile.mk
+++ profiles/shttpd/profile.mk
@@ -1,4 +1,5 @@
 
-WORLD_SUBDIRS_ZROUTER+=target/usr.sbin/shttpd
+#WORLD_SUBDIRS_ZROUTER+=target/usr.sbin/shttpd
+WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/www/shttpd
 
 WORLD_SUBDIRS+=secure/lib/libssl
