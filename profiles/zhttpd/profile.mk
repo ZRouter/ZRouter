5df0936494bfeb960dad5df38e6ea62b775a58b4
yamori813 <yamori813@yahoo.co.jp>
2018-11-23 18:02:58 +0900

    delete zhttpd

--- profiles/zhttpd/profile.mk
+++
@@ -1,6 +0,0 @@
-
-# XXX path must not be hardcoded, or must point to zhttpd in ports tree
-WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/www/libmicrohttpd
-WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/www/zhttpd
-
-
