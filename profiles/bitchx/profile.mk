a8271b730ac201c1299eca2043ed4949fb0f962e
yamori813 <yamori813@yahoo.co.jp>
2018-11-24 12:11:35 +0900

    add tcl for bitchx

--- profiles/bitchx/profile.mk
+++ profiles/bitchx/profile.mk
@@ -1,3 +1,4 @@
 WORLD_SUBDIRS+=secure/lib/libssl secure/lib/libcrypto
-WORLD_SUBDIRS_LIB+=ncurses/ncurses
+WORLD_SUBDIRS_LIB+=ncurses/ncurses libdl
+WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/lang/tcl85
 WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/irc/bitchx
