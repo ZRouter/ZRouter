e33e153e7a92206128cac815b834f83049740bf3
Oleksandr Rybalko <ray@ddteam.net>
2018-12-16 02:18:28 +0200

    Allow profile to build if it has Makefile.

--- profiles/profiles.mk
+++ profiles/profiles.mk
@@ -8,6 +8,11 @@
 .if exists(${ZROUTER_ROOT}/profiles/${profile}/) && exists(${ZROUTER_ROOT}/profiles/${profile}/profile.mk)
 .include "${ZROUTER_ROOT}/profiles/${profile}/profile.mk"
 
+.if exists(${ZROUTER_ROOT}/profiles/${profile}/Makefile)
+# If profile has Makefile, it seems want to be built. Adding it to subdirs.
+WORLD_SUBDIRS_ZROUTER+=profiles/${profile}
+.endif
+
 .if exists(${ZROUTER_ROOT}/profiles/${profile}/files)
 ROOTFS_COPY_DIRS+=${ZROUTER_ROOT}/profiles/${profile}/files
 .endif
