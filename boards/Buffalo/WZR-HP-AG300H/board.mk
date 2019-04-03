f6797ae22e33e9f6181f88544c1ce09c96a844bb
yamori813 <yamori813@yahoo.co.jp>
2019-04-03 15:44:46 +0900

    delete upgrade

--- boards/Buffalo/WZR-HP-AG300H/board.mk
+++ boards/Buffalo/WZR-HP-AG300H/board.mk
@@ -31,7 +31,7 @@ KERNCONF_MODULES_OVERRIDE+=	usb/uplcom usb/u3g usb/umodem usb/umass \
     usb/ucom cam zlib
 
 # Additional utilities
-WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
+#WORLD_SUBDIRS_ZROUTER+=target/sbin/upgrade
 
 KERNCONF_OPTIONS+=	DDB
 KERNCONF_OPTIONS+=	KDB
