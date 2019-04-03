fadc9dab6fd62d84e3c51540d1deb6fc27a657ec
yamori813 <yamori813@yahoo.co.jp>
2019-04-03 15:47:36 +0900

    add motion

---
+++ profiles/motion/profile.mk
@@ -0,0 +1,4 @@
+
+WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/www/libmicrohttpd
+WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/graphics/jpeg
+WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/multimedia/motion
