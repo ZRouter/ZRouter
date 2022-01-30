c557dab8d5375236abedcc05643cfda51c80051b
Hiroki Mori <yamori813@yahoo.co.jp>
2022-01-31 07:31:37 +0900

    only usb audio kernel object and add mixer

--- profiles/mpg123/profile.mk
+++ profiles/mpg123/profile.mk
@@ -1,4 +1,6 @@
 
 WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/audio/mpg123
 
-KERNCONF_MODULES_OVERRIDE+=sound
+KERNCONF_MODULES_OVERRIDE+=sound/driver/uaudio sound/sound
+
+WORLD_SUBDIRS_USR_SBIN+=mixer
