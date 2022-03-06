9da8c7126a93b91b6d09d0e8a48e390517427332
Hiroki Mori <yamori813@yahoo.co.jp>
2022-03-06 16:35:37 +0900

    openvpn move to ports from target

--- profiles/openvpn/profile.mk
+++ profiles/openvpn/profile.mk
@@ -1,8 +1,12 @@
 
-KERNCONF_MODULES_OVERRIDE+=if_tap
-WORLD_SUBDIRS+=		secure/lib/libcrypto secure/lib/libssl
-WORLD_SUBDIRS_ZROUTER+=	target/lib/liblzo2
-WORLD_SUBDIRS_ZROUTER+=	target/sbin/openvpn
+#KERNCONF_MODULES_OVERRIDE+=if_tap
+#WORLD_SUBDIRS+=		secure/lib/libcrypto secure/lib/libssl
+#WORLD_SUBDIRS_ZROUTER+=	target/lib/liblzo2
+#WORLD_SUBDIRS_ZROUTER+=	target/sbin/openvpn
 
 #WORLD_SUBDIRS_PORTS+=/usr/ports/security/openvpn
+
+KERNCONF_DEVICES+=tuntap
+WORLD_SUBDIRS+=secure/lib/libssl secure/lib/libcrypto
+WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/security/openvpn
 
