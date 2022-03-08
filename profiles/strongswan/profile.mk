5bcf2d87d51f3f5e40b37b0138bc9c5997921d81
Hiroki Mori <yamori813@yahoo.co.jp>
2022-03-08 11:51:57 +0900

    cleanup and add lib

--- profiles/strongswan/profile.mk
+++ profiles/strongswan/profile.mk
@@ -1,6 +1,7 @@
 
-#WORLD_SUBDIRS_LIB+= ../secure/lib/libcrypto ../secure/lib/libssl
-#WORLD_SUBDIRS_PORTS+=/usr/ports/security/racoon2
-#WORLD_SUBDIRS_ZROUTER+=target/sbin/racoon2
+KERNCONF_OPTIONS+=IPSEC
+KERNCONF_DEVICES+=enc crypto
+WORLD_SUBDIRS_LIB+=libexecinfo
+WORLD_SUBDIRS_LIB+=../secure/lib/libcrypto ../secure/lib/libssl
 WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/security/strongswan
 
