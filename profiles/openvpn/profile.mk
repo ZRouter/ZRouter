7f046018e621747cda840b1f6d1672eabf5f390f
Hiroki Mori <yamori813@yahoo.co.jp>
2022-03-10 10:28:41 +0900

    add libdl

--- profiles/openvpn/profile.mk
+++ profiles/openvpn/profile.mk
@@ -1,7 +1,7 @@
 
 KERNCONF_DEVICES+=tuntap
 
-WORLD_SUBDIRS_LIB+=../secure/lib/libssl ../secure/lib/libcrypto
+WORLD_SUBDIRS_LIB+=../secure/lib/libssl ../secure/lib/libcrypto libdl
 WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/archivers/lzo2
 WORLD_SUBDIRS_PORTS+=${ZROUTER_ROOT}/ports/security/openvpn
 
