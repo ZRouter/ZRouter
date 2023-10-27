93b4f6ca007ad0ed73b79ce8bde3ceb0df150ca5
Hiroki Mori <yamori813@yahoo.co.jp>
2023-10-27 14:17:02 +0900

    add gpioiic config by comment

--- boards/Buffalo/WHR-G300N/board.mk
+++ boards/Buffalo/WHR-G300N/board.mk
@@ -27,6 +27,8 @@ KERNCONF_OPTIONS+=     ROOTDEVNAME=\\\"cd9660:da0\\\"
 KERNCONF_OPTIONS+=     ROOTDEVNAME=\\\"cd9660:cfid0s.rootfs.uzip\\\" 
 .endif
 
+#KERNCONF_DEVICES+=	gpioiic
+#KERNCONF_DEVICES+=	iicbb
 KERNCONF_DEVICES+=	mtk_iic
 KERNCONF_DEVICES+=	iicbus
 KERNCONF_DEVICES+=	iic
