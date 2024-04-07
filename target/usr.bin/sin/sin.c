08ff9117d7c541cdb89005009664474cf6f394a2
Hiroki Mori <yamori813@yahoo.co.jp>
2024-04-07 18:01:31 +0900

    delete japanese comment and use mache endian

--- target/usr.bin/sin/sin.c
+++ target/usr.bin/sin/sin.c
@@ -11,34 +11,24 @@
 #include <unistd.h>
 
 /*
- * 量子化ビット数 : 16 bits
- * サンプリング周波数 : 44.1 KHz
- * チャンネル数 : 1
- * 再生時間 : 5 sec
- *
- * 分のバッファ領域
- *
- * 16 (bits) * 44100 (Hz) * 5 (sec) = 3528000 (bits)
- *                                  = 441000  (bytes)
+ * 16 (bits) * 48000 (Hz) * 1 (sec) = 96000 (bytes)
  */
-#define BUFSIZE 480000
+ 
+#define BUFSIZE 96000
 
-static int setup_dsp( int fd, int be );
+static int setup_dsp( int fd);
 
 int 
 main(int argc, char ** argv)
 {
-	/* 再生時間 5 秒 */
-	double total = 5.0, t; 
+	double total = 1.0, t; 
 
-	/* 再生音周波数 440 Hz ( A ) */
 	double freq = 440.0;
 
 	short  buf[ BUFSIZE / sizeof(short) ];
 	int    fd, i;
 	char   ch;
 	int    silence = 0;
-	int    bigendian = 0;
 
 	while ((ch = getopt(argc, argv, "skb")) != -1) {
 		if(ch == 's') {
@@ -47,9 +37,6 @@ main(int argc, char ** argv)
 		if(ch == 'k') {
 			freq = 1000.0;
 		}
-		if(ch == 'b') {
-			bigendian = 1;
-		}
 	}
 
 	if ( ( fd = open( "/dev/dsp", O_WRONLY ) ) == -1 ) {
@@ -57,16 +44,13 @@ main(int argc, char ** argv)
 		return 1;
 	}
 
-
-	/* /dev/dsp の設定 */
-	if ( setup_dsp( fd, bigendian ) != 0 ) {
+	if ( setup_dsp( fd) != 0 ) {
 		fprintf( stderr, "Setup /dev/dsp failed.\n" );
 		close( fd );
 		return 1;
 	}
 
 
-	/* 再生用の正弦波データ作成 */
 	for ( i = 0; i < BUFSIZE / sizeof(short); i ++ ) {
 		t = ( total / (BUFSIZE / sizeof(short)) ) * i;
 		if (silence == 1)
@@ -88,38 +72,29 @@ main(int argc, char ** argv)
 }
 
 
-/*
- * /dev/dsp を以下の様に設定する。
- *
- * 量子化ビット数     : 16   bits
- * サンプリング周波数 : 44.1 KHz
- * チャンネル数       : 1
- * PCM データは符号付き、リトルエンディアン
- *
- */
 static int
-setup_dsp( int fd, int be )
+setup_dsp( int fd)
 {
-	int fmt     = AFMT_S16_LE;
+	int fmt;
 	int freq    = 48000;
 	int channel = 1;
 
-	/* サウンドフォーマットの設定 */
-	if ( be )
-		fmt = AFMT_S16_BE;
+#if BYTE_ORDER == LITTLE_ENDIAN
+	fmt = AFMT_S16_LE;
+#else
+	fmt = AFMT_S16_BE;
+#endif
 
 	if ( ioctl( fd, SOUND_PCM_SETFMT, &fmt ) == -1 ) {
 		perror( "ioctl( SOUND_PCM_SETFMT )" );
 		return -1;
 	}
 
-	/* チャンネル数の設定 */
 	if ( ioctl( fd, SOUND_PCM_WRITE_CHANNELS, &channel ) == -1 ) {
 		perror( "iotcl( SOUND_PCM_WRITE_CHANNELS )" );
 		return -1;
 	}
 
-	/* サンプリング周波数の設定 */
 	if ( ioctl( fd, SOUND_PCM_WRITE_RATE, &freq ) == -1 ) {
 		perror( "iotcl( SOUND_PCM_WRITE_RATE )" );
 		return -1;
@@ -127,6 +102,3 @@ setup_dsp( int fd, int be )
 
 	return 0;
 }
-
-/* End of sound.c */
-
